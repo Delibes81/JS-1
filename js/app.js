fe6d301e94b161974a6f98c44a75e4c218ebd761
Delibes <delibes.ats@gmail.com>
2024-06-22 01:58:49 -0600

    Commit app avance 2

---
+++ js/app.js
@@ -0,0 +1,37 @@
+let egresos = {
+    Renta: 900,
+    Ropa:400
+}
+
+let ingresos = {
+    Quincena: 9000,
+    Venta: 400,
+}
+
+const cargarCabecero = () => {
+    let presupuesto = totalIngresos() - totalEgresos()
+    let porcentajeEgreso = (totalIngresos() -  totalEgresos()) / totalIngresos()    
+}
+
+const totalIngresos = () => {
+    let total = 0
+    for (const key in ingresos) {
+        const value = ingresos[key]
+        total = total + value
+    }
+    return total
+}
+
+const totalEgresos = () => {
+    let total = 0
+    for (const key in egresos) {
+        const value = egresos[key]
+        total = total + value
+    }
+    return total
+}
+
+const formatoMoneda = (valor) => `$${valor.toLocaleString()}`
+const formatoPorcentaje = (valor) => `${valor.toLocaleString()}%`
+
+cargarCabecero()
